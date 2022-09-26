08a2896bdf11682c46057a36e421f5ac9fc617bd
Marcos Hideki <jorge.sakaguchi@outlook.com>
2022-09-26 17:34:57 +0000

    Pqp era só fazer o teste

---
+++ teste.java
@@ -0,0 +1,9 @@
+public class teste {
+    public static void main(String[]args){
+        Paralelepipedo bola = new Paralelepipedo();
+
+        bola.CalculaAreaTotal();
+        bola.CalculaFace();
+        bola.CalculaVolume();
+    }
+}
