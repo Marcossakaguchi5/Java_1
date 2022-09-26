97570d76199771eb096a036ffddc73b7eebbfde1
Marcos Hideki <jorge.sakaguchi@outlook.com>
2022-09-26 17:34:34 +0000

    Tinha que ter feito o teste, Burrão

---
+++ ParalelepipedoTeste.java
@@ -0,0 +1,11 @@
+class ParalelepipedoTeste {
+    public static void main(String[] args){
+        ParalelepipedoCorrigido p = new ParalelepipedoCorrigido(3, 4, 2);
+        
+        System.out.println("Volume: "+p.calculaVolume());
+        System.out.println("Area da face 1: "+p.calculaAreaFace(1));
+        System.out.println("Area da face 2: "+p.calculaAreaFace(2));
+        System.out.println("Area da face 3: "+p.calculaAreaFace(3));
+        System.out.println("Volume: "+p.calculaAreaTotal());
+    }
+}
